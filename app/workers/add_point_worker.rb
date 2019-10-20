bcd5dbee55d1008cf004df4183a93a7adff8a876
Mateus Athaydes Martins <matt.athaydes@gmail.com>
2019-10-20 09:31:52 -0300

    hotfix

--- app/workers/add_point_worker.rb
+++ app/workers/add_point_worker.rb
@@ -7,7 +7,6 @@ class AddPointWorker
     if user.used_invitation_code.present?
       invitee_user = User.find_by(invitation_code: user.used_invitation_code)
       if invitee_user && invitee_user.actual_phase.step.to_s == '1'
-        binding.pry
         new_points = invitee_user.reward_points += 1
         invitee_user.reward_points = new_points
         invitee_user.save
