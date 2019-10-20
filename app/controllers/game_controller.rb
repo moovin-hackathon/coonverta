a56235ec2a29579cae194df11f7b74f3bd6250db
Mateus Athaydes Martins <matt.athaydes@gmail.com>
2019-10-20 09:38:58 -0300

    hotfix

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,6 +1,6 @@
 class GameController < ApplicationController
     layout 'play'
-    def 
+    def play
         @phase = current_user.actual_phase
         @invited_friends = @current_user.invited_friends.pluck(:name) if @phase.step == '1'
         @user_invitation_code = @current_user.invitation_code
